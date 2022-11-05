d86afda34023179dbfb2ad55ac839a915eb244e1
AdinataKW <adinata2862@gmail.com>
2022-11-05 14:00:57 +0700

    menyelesaikankan vid 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,18 @@
 @extends('layout.main')
+
 @section('container')
+
 <article class="mb-5">
     <h2>{{ $post->title }}</h2>
-    <h5>{{ $post->author }}</h5>
+
     <p>By. ACIL in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">
+        {{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
+
 <a href="/blog">back to posts</a>
+<a class="d-block mt-3" href="/blog">back to posts</a>
+
+
 @endsection
